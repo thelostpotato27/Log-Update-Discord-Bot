571e02e39c974404c03d8e993df7e4790848f45c
Jeffrey Chang <Thelostpotato27@gmail.com>
2022-02-03 10:26:27 -0600

    desktop push

--- example_bot.py
+++ example_bot.py
@@ -1,17 +1,44 @@
 import discord
+import csv
+import gspread
+import pandas as pd
+from oauth2client.service_account import ServiceAccountCredentials
 
-client = discord.Client()
+scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+creds = ServiceAccountCredentials.from_json_keyfile_name('add_json_file_here.json', scope)
+sheet_client = gspread.authorize(creds)
 
-@client.event
+sheet = sheet_client.open('commentary data')
+sheet_instance = sheet.get_worksheet(0)
+
+all_students = []
+for x in range(sheet_instance.row_count):
+    full_name = sheet_instance.cell(col=1,row=x+1)
+    subject = sheet_instance.cell(col=2,row=x+1)
+    date = sheet_instance.cell(col=3,row=x+1)
+    student_data = [full_name, subject, date]
+    all_students.append(student_data)
+
+
+
+
+discord_client = discord.Client()
+
+
+@discord_client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {0.user}'.format(discord_client))
 
-@client.event
+
+@discord_client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == discord_client.user:           #checks that it's not responding to itself
         return
 
-    if message.content.startswith('$hello'):
+    if message.content.startswith('$Taught'):   #checks that msg starts with $Taught
+        words = message.content.split()
+        full_name = words[1] + words[2]
+        
         await message.channel.send('Hello!')
 
-client.run('put token here')
+discord_client.run('put token here')
