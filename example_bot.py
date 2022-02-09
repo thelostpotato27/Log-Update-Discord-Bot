e5622e19db4218c31d09afd5206898fc80fef714
Jeffrey <thelostpotato27@gmail.com>
2022-02-08 23:01:24 -0600

    adding csv reading and writing capabilities

--- example_bot.py
+++ example_bot.py
@@ -11,20 +11,21 @@ discord_client = discord.Client()
 recorded_logs_channel = discord.utils.get(discord_client.get_all_channels(), name="recorded-logs")
 class_logs_channel = discord.utils.get(discord_client.get_all_channels(), name="class-logs")
 channel_id = recorded_logs_channel.id
-class_logs = "temp"
-logs_message = "temp"
-final_owed = "temp"
-final_owed_message = "temp"
+class_logs = "temp"         #.csv file holding class_logs
+logs_message = "temp"       #specific message that holds the class_logs file
+final_owed = "temp"         #.csv file holding final amounts owed
+final_owed_message = "temp" #specific message holding amount owed file
+
 @discord_client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(discord_client))
-    async for m in discord_client.logs_from(channel):
+    async for m in discord_client.logs_from(recorded_logs_channel):
         if m.content() == "name of class logs":
-            class_logs = request(GET, m.attachments[0].url)
+            class_logs = discord.request(discord.GET, m.attachments[0].url)
             logs_message = m
             print('class_logs found'.format(discord_client))
         if m.content() == "name of final owed":
-            final_owed = request(GET, m.attachments[0].url)
+            final_owed = discord.request(discord.GET, m.attachments[0].url)
             final_owed_message = m
             print('final_owed found'.format(discord_client))
     print('Finished!'.format(discord_client))
@@ -61,10 +62,15 @@ async def on_message(message):
         full_name = words[1] + '_' + words[2]
         subject = words[3]
         class_cost = "temp"                     #need to make a csv file to store the prices of certain class types
-        with open(class_logs, 'w', newline='') as csvfile:
+        with open(class_logs, 'w', newline='') as csvfile:      #change to DictWriter, makes it easier to parse through
             csv_w_class_logs = csv.writer(csvfile, delimiter=' ')
             csv_w_class_logs.writerow([date] + [time] + [full_name] + [subject] + [class_cost])
-        await send(file = class_logs)
+        await recorded_logs_channel.send(file = class_logs)
+
+        with open(final_owed, 'w', newline='') as csvfile:      #final owed is a Dictcsv of Name, Amount Due
+            csv_r_final_owed = csv.DictReader(csvfile)
+
+
 
         await 
         csv_w_final_owed = csv.writer(final_owed, delimiter=' ')
