64f6fce82878b912e59edf866bccfa5d8b446d63
Jeffrey Chang <Thelostpotato27@gmail.com>
2022-06-05 06:57:41 -0700

    fixed bugs regarding reading .csv, having trouble with replacing .csv files now

--- example_bot.py
+++ example_bot.py
@@ -8,19 +8,19 @@ from tempfile import NamedTemporaryFile
 
 bot = commands.Bot(command_prefix='$')
 
-class_logs = "temp"         #.csv file holding class_logs
-logs_message = "temp"       #specific message that holds the class_logs file
-final_owed = "temp"         #.csv file holding final amounts owed
-final_owed_message = "temp" #specific message holding amount owed file
-class_cost = "temp"         #.csv file holding class_cost list
-class_cost_message = "temp" #specific message holding class cost file
+bot.class_logs = "temp"         #.csv file holding class_logs
+bot.logs_message = "temp"       #specific message that holds the class_logs file
+bot.final_owed = "temp"         #.csv file holding final amounts owed
+bot.final_owed_message = "temp" #specific message holding amount owed file
+bot.class_cost = "temp"         #.csv file holding class_cost list
+bot.class_cost_message = "temp" #specific message holding class cost file
 
 
 # internal class logs file, stored in .csv format. Instead of redownloading the .csv file to update it, the bot will download it once and
 # maintain its internal copy. Whenever an update is made, the bot will replace the current .csv file with its current internal copy
-class_logs_internal = NamedTemporaryFile(mode='w', delete=False)
+bot.class_logs_internal = NamedTemporaryFile(mode='w', delete=False)
 fields = ['date', 'time', 'first_name', 'last_name', 'subject', 'amount_owed']
-class_logs_writer = csv.DictWriter(class_logs_internal, fieldnames=fields)
+class_logs_writer = csv.DictWriter(bot.class_logs_internal, fieldnames=fields)
 
 #On start, open recorded-logs channel and parse through messages, if message has an attachment with the 
 #proper names, assign those attachments to a variable and print that the proper message has been found
@@ -42,16 +42,16 @@ async def on_ready():
             temp_holder = temp_holder.replace('\r', '')
             temp_holder = temp_holder.split(',')
             class_logs_size = int(len(temp_holder)/6)
-            class_logs = ['temp'] * (class_logs_size-1)
+            bot.class_logs = ['temp'] * (class_logs_size-1)
             for i in range(1, class_logs_size):
                 starting_position = 6*i
-                class_logs[i-1] = [temp_holder[starting_position], temp_holder[starting_position+1], temp_holder[starting_position+2],temp_holder[starting_position+3], temp_holder[starting_position+4], temp_holder[starting_position+5]]
-                new_row = {'date': class_logs[i-1][0], 'time': class_logs[i-1][1], 'first_name': class_logs[i-1][2], 'last_name': class_logs[i-1][3], 'subject': class_logs[i-1][4], 'amount_owed': class_logs[i-1][5]}
+                bot.class_logs[i-1] = [temp_holder[starting_position], temp_holder[starting_position+1], temp_holder[starting_position+2],temp_holder[starting_position+3], temp_holder[starting_position+4], temp_holder[starting_position+5]]
+                new_row = {'date': bot.class_logs[i-1][0], 'time': bot.class_logs[i-1][1], 'first_name': bot.class_logs[i-1][2], 'last_name': bot.class_logs[i-1][3], 'subject': bot.class_logs[i-1][4], 'amount_owed': bot.class_logs[i-1][5]}
                 class_logs_writer.writerow(new_row)
 
-            logs_message = m
+            bot.logs_message = m
             print('class_logs found'.format(bot))
-            print(class_logs)
+            print(bot.class_logs)
 
         # final owed format is [First_name, Last_name, Amount Due]
         if m.attachments != [] and m.attachments[0].filename == "final_owed.csv":
@@ -61,25 +61,25 @@ async def on_ready():
             temp_holder = temp_holder.replace('\r', '')
             temp_holder = temp_holder.split(',')
             final_owed_size = int(len(temp_holder)/3)
-            final_owed = ['temp'] * (final_owed_size-1)
+            bot.final_owed = ['temp'] * (final_owed_size-1)
             for i in range(1, final_owed_size):
                 starting_position = 3*i
-                final_owed[i-1] = [temp_holder[starting_position], temp_holder[starting_position+1], temp_holder[starting_position+2]]
-            final_owed_message = m
+                bot.final_owed[i-1] = [temp_holder[starting_position], temp_holder[starting_position+1], temp_holder[starting_position+2]]
+            bot.final_owed_message = m
             print('final_owed found'.format(bot))
-            print(final_owed)
+            print(bot.final_owed)
         
         # class cost format is [Subject, Amount]
         if m.attachments != [] and m.attachments[0].filename == "class_cost.csv":
-            class_cost = await m.attachments[0].read()
-            class_cost = class_cost.decode("utf-8", "strict")
-            class_cost = class_cost.replace('\n', '')
-            class_cost = class_cost.replace('\r', '')
-            class_cost = class_cost.split(',')
-            class_cost = class_cost[2:]
-            class_cost_message = m
+            temp_cost = await m.attachments[0].read()
+            temp_cost = temp_cost.decode("utf-8", "strict")
+            temp_cost = temp_cost.replace('\n', '')
+            temp_cost = temp_cost.replace('\r', '')
+            temp_cost = temp_cost.split(',')
+            bot.class_cost = temp_cost[2:]
+            bot.class_cost_message = m
             print('class_cost found'.format(bot))
-            print(class_cost)
+            print(bot.class_cost)
     print('Finished!'.format(bot))
 
 
@@ -140,14 +140,16 @@ async def taught(ctx, first_name, last_name, subject, time, date):
     #takes the subject of the class taught and returns the price of the class taught
     #class_cost csv file format is [subject, amount]
     amount_owed = 0
-    for i in range(len(class_cost)):
-        if subject == class_cost[i]:
-            amount_owed = class_cost[i + 1]
+    print("input subject is " + subject)
+    for i in range(len(bot.class_cost)):
+        print("Checked subject: " + bot.class_cost[i])
+        if subject == bot.class_cost[i]:
+            amount_owed = bot.class_cost[i + 1]
             break
     if amount_owed == 0:
         stop_command = 0
         print("Subject does not exist, possible subjects:")
-        for elem in class_cost:
+        for elem in bot.class_cost:
             if subject[0] == elem[0]:
                 print(elem)
 
@@ -161,7 +163,15 @@ async def taught(ctx, first_name, last_name, subject, time, date):
 
         writer = csv.DictWriter(tempfile, fieldnames=fields)
         name_seen_flag = 0
-        for row in final_owed:
+        print("First name inputed: ", end = '')
+        print(first_name)
+        print("Last name input: ", end = '')
+        print(last_name)
+        for row in bot.final_owed:
+            print("Row in final owed:")
+            print(row)
+            print("row[0] is ", end = '')
+            print(row[0])
             if row[0] == first_name:
                 if row[1] == last_name:
                     print('updating row',full_name.format(bot))
@@ -171,27 +181,27 @@ async def taught(ctx, first_name, last_name, subject, time, date):
             writer.writerow(new_row)
         if name_seen_flag:
             # update 2D array class_logs and add a new row to the internal .csv file
-            class_logs.append([date, time, first_name , last_name, subject, amount_owed])
-            new_row = {'date': class_logs[-1][0], 'time': class_logs[-1][1], 'first_name': class_logs[-1][2], 'last_name': class_logs[-1][3], 'subject': class_logs[-1][4], 'amount_owed': class_logs[-1][5]}
+            bot.class_logs.append([date, time, first_name , last_name, subject, amount_owed])
+            new_row = {'date': bot.class_logs[-1][0], 'time': bot.class_logs[-1][1], 'first_name': bot.class_logs[-1][2], 'last_name': bot.class_logs[-1][3], 'subject': bot.class_logs[-1][4], 'amount_owed': bot.class_logs[-1][5]}
             class_logs_writer.writerow(new_row)
         else:
             print("Name does not exist, please check for spelling mistakes. Potential names:".format(bot))
             #go through all names here and suggest names that either have the same first name spelling, same last name spelling, only have one letter mispelled,
             #or have the same first letter of first or last name and +-2 on the length of the first or last name
-            for row in final_owed:
+            for row in bot.final_owed:
                 #prints name if same length and has 2 spelling mistakes or less
-                if first_name.len() == row[0].len():
+                if len(first_name) == len(row[0]):
                     mispelled = 0
-                    for i in range(first_name.len()):
+                    for i in range(len(first_name)):
                         if first_name[i] != row[0][i]: mispelled += 1
                     if mispelled <= 2:
                         print(row[0].format(bot))
                         print(row[1].format(bot))
                         continue
                 
-                if last_name.len() == row[1].len():
+                if len(last_name) == len(row[1]):
                     mispelled = 0
-                    for i in range(last_name.len()):
+                    for i in range(len(last_name)):
                         if last_name[i] != row[1][i]: mispelled += 1
                     if mispelled <= 2:
                         print(row[0].format(bot))
@@ -199,13 +209,13 @@ async def taught(ctx, first_name, last_name, subject, time, date):
                         continue
                 
                 #prints name if have the same first starting letter and also have a name length difference of 2 or less
-                if first_name.len() >= row[0].len()-1 or first_name.len() <= row[0].len()+1:
+                if len(first_name) >= len(row[0])-1 or len(first_name) <= len(row[0])+1:
                     if first_name[0] == row[0][0]:
                         print(row[0].format(bot))
                         print(row[1].format(bot))
                         continue
 
-                if last_name.len() >= row[1].len()-1 or last_name.len() <= row[1].len()+1:
+                if len(last_name) >= len(row[1])-1 or len(last_name) <= len(row[1])+1:
                     if last_name[0] == row[1][0]:
                         print(row[1].format(bot))
                         print(row[1].format(bot))
@@ -213,8 +223,8 @@ async def taught(ctx, first_name, last_name, subject, time, date):
 
             return
         # shutil.move(tempfile.name, final_owed)
-        await final_owed_message.edit(embed = tempfile)
-        await logs_message.edit(embed = class_logs_internal)
+        await bot.final_owed_message.edit(embed = tempfile)
+        await bot.logs_message.edit(embed = bot.class_logs_internal)
 
 
-bot.run('add token here')
+bot.run('token here')
